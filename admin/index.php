c1e750d4f832100d48e5cbe1db1a2ef28482505c
infosepp <71786937+infosepp@users.noreply.github.com>
2025-01-20 16:07:18 +0100

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * Dieses Werk ist lizenziert unter der Creative Commons Lizenz:
+ * Namensnennung - Nicht kommerziell - Weitergabe unter gleichen Bedingungen 4.0 International (CC BY-NC-SA 4.0).
+ * Siehe die Datei 'license.txt' für Details.
+ */
 // Optional: Weiterleitung auf die Login-Seite
 header('Location: login.php');
 exit;
